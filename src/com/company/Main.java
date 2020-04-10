2f2d6bdc3999876667481543c90d9e49c486372f
Ch.D <chumakov.d1@gmail.com>
2020-04-10 15:26:49 +0300

    added filereading

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         FileWriter fw = new FileWriter("output.txt");
         String[] peopleinfo = new String[10];
-        
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
+
+            int temp=0;
+            String s="";
+            while ((s = br.readLine()) != null) {
+                peopleinfo[temp]=s;
+                temp++;
+            }
+        }
+
+        for(String temp:peopleinfo){
+            System.out.println(temp);
+        }
 
     }
 }
