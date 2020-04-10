b94b26d22a4b17b04ea426f1e33090a088c3bd9f
Ch.D <chumakov.d1@gmail.com>
2020-04-10 15:22:13 +0300

    added libraries and files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,15 @@
 package com.company;
-
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileWriter fw = new FileWriter("output.txt");
+        String[] peopleinfo = new String[10];
+        
 
     }
 }
